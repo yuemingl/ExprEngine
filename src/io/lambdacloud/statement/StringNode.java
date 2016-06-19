27c575e84eb62e36d86f752947fd0feb01f23341
Yueming Liu <nkliuyueming@gmail.com>
2016-06-18 23:53:50 -0700

    Add StringNode

--- src/io/lambdacloud/statement/StringNode.java
+++ src/io/lambdacloud/statement/StringNode.java
@@ -1,5 +1,24 @@
 package io.lambdacloud.statement;
 
-public class StringNode {
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Type;
 
+public class StringNode extends ExprNode {
+	String str;
+	public StringNode(String s) {
+		this.str = s;
+	}
+	@Override
+	public void genCode(MethodVisitor mv) {
+		mv.visitLdcInsn(str);
+	}
+	
+	public static String test() {
+		return "abc";
+	}
+	
+	@Override
+	public Type getType() {
+		return Type.getType(String.class);
+	}	
 }
