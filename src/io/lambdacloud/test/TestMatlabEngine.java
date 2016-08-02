27c04ae027f7c7704daddf5556576535bd349bf9
Nathan Liu <cs_opo@hotmail.com>
2016-08-01 23:14:23 -0700

    Use assertEqual for all the test cases

--- src/io/lambdacloud/test/TestMatlabEngine.java
+++ src/io/lambdacloud/test/TestMatlabEngine.java
@@ -11,59 +11,61 @@ import Jama.Matrix;
 
 public class TestMatlabEngine {
 	public static void main(String[] args){
-		exec("function myfun(a, b)\n c=a+b\n d=a-b\n [c d]\n end\n myfun(10,100)");
-		
 		test();
 	}
 	
 	public static void test() {
-		Matrix m = new Matrix(new double[][]{{1,2},{3,4}});
-		Matrix n = new Matrix(new double[][]{{10,20},{30,40}});
-
 		double[][] array = {{1.,2.,3},{4.,5.,6.},{7.,8.,10.}};
 		double[][] array2 = {{1.,2.,3},{1.,1.,1.},{2.,2.,2.}};
 		Matrix A = new Matrix(array);
 		Matrix B = new Matrix(array2);
 		Matrix b = Matrix.random(3,1);
 		
-		exec("function myfun(a, b)\n c=a+b\n d=a-b\n [c d]\n end\n myfun(10,100)");
-		//exec("function [c d]=myfun(a, b)\n c=a+b\n d=a-b\n end\n myfun(10,100)");
-		//exec("function [c d]=myfun(a, b)\n c=a+b\n d=a-b\n end\n myfun(10.,100.)");
-		//exec("function myfun(a, b)\n c=a+b\n d=a-b\n [c d]\n end\n myfun(10.,100.)");
+		Matrix M = new Matrix(new double[][]{{1,2},{3,4}});
+		Matrix N = new Matrix(new double[][]{{10,20},{30,40}});
+		Matrix d = getMatrix(3,4);
 		
-		//exec("function myfun(a)\n a+1\n a+2; end\n myfun(1)");
-		//exec("function myfun(a)\n a+1\n a+2\n end\n myfun(1)");
-		//exec("function a=myfun(a)\n a+1\n a+2\n end\n myfun(1);");
-		//exec("function a=myfun(a)\n a+1\n a+2\n end\n myfun(1)");
+		assertEqual(exec("function myfun(a, b)\n c=a+b\n d=a-b\n [c d]\n end\n myfun(10,100)"), getMatrix(110,-90));
+		assertEqual(exec("function [c d]=myfun(a, b)\n c=a+b\n d=a-b\n end\n myfun(10,100)"), getMatrix(110,-90));
+		assertEqual(exec("function [c d]=myfun(a, b)\n c=a+b\n d=a-b\n end\n myfun(10.,100.)"), getMatrix(110,-90));
+		assertEqual(exec("function myfun(a, b)\n c=a+b\n d=a-b\n [c d]\n end\n myfun(10.,100.)"), getMatrix(110,-90));
 		
-//		exec("A=[1 2; 3 4]; b=[3 5]; A\\b");
-//		exec("A=[1 2; 3 4]\n b=[3 5]\n A\\b");
+		assertEqual(exec("function myfun(a)\n a+1\n a+2; end\n myfun(1)"), 3);
+		assertEqual(exec("function myfun(a)\n a+1\n a+2\n end\n myfun(1)"), 3);
+		assertEqual(exec("function a=myfun(a)\n a+1\n a+2\n end\n myfun(1);"), 1);
+		assertEqual(exec("function a=myfun(a)\n a+1\n a+2\n end\n myfun(1)"), 1);
 		
-//		exec("a=2\n a");
-//		exec("a=2; a");
-//		exec("100+1; 200+1;");
-//		exec("10+1\n 20+1");
+		assertEqual(exec("M=[1 2; 3 4]; d=[3 4]; M\\d"), M.solve(d));
+		assertEqual(exec("M=[1 2; 3 4]\n d=[3 4]\n M\\d"), M.solve(d));
+		
+		assertEqual(exec("a=2\n a"), 2);
+		assertEqual(exec("a=2; a"), 2);
+		assertEqual(exec("100+1; 200+1;"), 201);
+		assertEqual(exec("10+1\n 20+1"), 21);
 
-//		myPrint(exec("function [c d] = myfun(a, b)\nc=a+b; d=a-b;a;b;a+1\nend\nmyfun(10,100)"));
-//		myPrint(exec("function [c d] = myfun(a, b)\nc=a+b; d=a-b;\nend\nmyfun(10,100)"));
-//		myPrint(exec("function c = myfun(a, b)\na+b\nend\nmyfun(10,100)"));
-//		myPrint(exec("fun(1, 1)"));
-//		myPrint(exec("a=[1, 2; 3, 4];  a(1, 1)"));
-//		
-//		myPrint(exec("a=[1 2; 3 4];  a(1, 1)"));
-//		myPrint(exec("a=[1 2; 3 4];  a(1,1)"));
-//		
-////		//white space test
-////		//myPrint(exec("a=[1 2; 3 4];b=[2 3; 4 5]; a+b"));
-////		myPrint(exec("a=[1 2; 3 4];  b=[2 3; 4 5]; a+b"));
-//		myPrint(exec("a=  [1 2; 3 4]; a +  [2 3; 4 5]  ;  "));
-//
+		assertEqual(exec("function [c d] = myfun(a, b)\nc=a+b; d=a-b;a;b;a+1\nend\nmyfun(10,100)"), getMatrix(110,-90));
+		assertEqual(exec("function [c d] = myfun(a, b)\nc=a+b; d=a-b;\nend\nmyfun(10,100)"), getMatrix(110,-90));
+		assertEqual(exec("function c = myfun(a, b)\nc=a+b\nend\nmyfun(10,100)"),110);
 		
-//		myPrint(exec("A(0:2,1:2)+B(0:2,1:2)", getMap("A",A,"B",B)));
-//		myPrint(exec("A(1:2,1:2)-B(1:2,1:2)", getMap("A",A,"B",B)));
-//		myPrint(exec("A(1:2,1:2)*B(1:2,1:2)", getMap("A",A,"B",B)));
-//		myPrint(exec("A(1:2,1:2).*B(1:2,1:2)", getMap("A",A,"B",B)));
-//		myPrint(exec("A(1:2,1:2)./B(1:2,1:2)", getMap("A",A,"B",B)));
+//		myPrint(exec("fun(1, 1)"));
+		assertEqual(exec("a=[1, 2; 3, 4];  a(1, 1)"), getMatrix(4));
+		assertEqual(exec("a=[1 2; 3 4];  a(1, 1)"), getMatrix(4));
+		assertEqual(exec("a=[1 2; 3 4];  a(1,1)"), getMatrix(4));
+		
+		//white space test
+		assertEqual(exec("A=[1 2; 3 4];B=[10 20; 30 40]; A+B"), M.plus(N));
+		assertEqual(exec("A=[1 2; 3 4];  B=[10 20; 30 40]; A+B"), M.plus(N));
+		assertEqual(exec("A=  [1 2; 3 4]; A +  [10 20; 30 40]  ;  "), M.plus(N));
+
+		Matrix A1 = A.getMatrix(0,2,1,2);
+		Matrix A2 = A.getMatrix(1,2,1,2);
+		Matrix B1 = B.getMatrix(0,2,1,2);
+		Matrix B2 = B.getMatrix(1,2,1,2);
+		assertEqual(exec("A(0:2,1:2)+B(0:2,1:2)",  getMap("A",A,"B",B)), A1.plus(B1));
+		assertEqual(exec("A(1:2,1:2)-B(1:2,1:2)",  getMap("A",A,"B",B)), A2.minus(B2));
+		assertEqual(exec("A(1:2,1:2)*B(1:2,1:2)",  getMap("A",A,"B",B)), A2.times(B2));
+		assertEqual(exec("A(1:2,1:2).*B(1:2,1:2)", getMap("A",A,"B",B)), A2.arrayTimes(B2));
+		assertEqual(exec("A(1:2,1:2)./B(1:2,1:2)", getMap("A",A,"B",B)), A2.arrayRightDivide(B2));
 		
 		assertEqual(exec("-A",    getMap("A",A)),       A.uminus());
 		assertEqual(exec("A+B",   getMap("A",A,"B",B)), A.plus(B));
@@ -75,12 +77,12 @@ public class TestMatlabEngine {
 		assertEqual(exec("A.\\B", getMap("A",A,"B",B)), A.arrayLeftDivide(B));
 		assertEqual(exec("A./B",  getMap("A",A,"B",B)), A.arrayRightDivide(B));
 		
-		
+		//no longer supported for array
+		//myPrint(exec("A(1,1:2)", getMap("A",array)));
 		//myPrint(exec("A(1,1:2)", getMap("A",array)));
 		
-		//myPrint(exec("A(1,1:2)", getMap("A",array)));
-		//myPrint(exec("A(1:2,1:2)", getMap("A",A)));
-		//myPrint(exec("A(1:2,2)", getMap("A",A)));
+		assertEqual(exec("A(2,1:2)", getMap("A",A)), A.getMatrix(2,2,1,2));
+		assertEqual(exec("A(1:2,2)", getMap("A",A)), A.getMatrix(1,2,2,2));
 		
 		assertEqual(exec("[1. .2 3.]"), getMatrix(1,0.2,3));
 		
@@ -88,9 +90,9 @@ public class TestMatlabEngine {
 		
 		assertEqual(exec("[1 2 3; 4 5 6; 7 8 10]"), A);
 		assertEqual(exec("[1 2 3; 4 5 6; 7 8 10]'"), A.transpose());
-		assertEqual(exec("A'", getMap("A",m)), m.transpose());
+		assertEqual(exec("A'", getMap("A",M)), M.transpose());
 
-		assertEqual(exec("a + b  ", getMap("a",m,"b",n)), m.plus(n));
+		assertEqual(exec("a + b  ", getMap("a",M,"b",N)), M.plus(N));
 		assertEqual(exec("a+b", getMap("a",2,"b",3)), 5);
 		
 		
