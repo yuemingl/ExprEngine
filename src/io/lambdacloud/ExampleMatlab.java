2ac07dc987743e1d74123dfd8d77c8462b610e53
Nathan Liu <cs_opo@hotmail.com>
2016-08-06 23:37:37 -0700

    Add examples

--- src/io/lambdacloud/ExampleMatlab.java
+++ src/io/lambdacloud/ExampleMatlab.java
@@ -8,10 +8,6 @@ public class ExampleMatlab {
 		System.out.println("Computing Fibonacci(46): ");
 		MatlabEngine.exec("function fib(n) if n<=1; 1L; else fib(n-1)+fib(n-2); end end"
 				+ " tic; fib(46)\n toc");
-		
-		MatlabEngine.exec("tic; toc; 1 ");
-		MatlabEngine.exec("tic; 9+10\n toc; 1 ");
-
 	}
 
 }
