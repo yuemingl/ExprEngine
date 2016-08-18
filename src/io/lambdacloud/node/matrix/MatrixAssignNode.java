dd0f37abb4997bc6de35fa60281e49e89796b80e
Nathan Liu <cs_opo@hotmail.com>
2016-08-18 00:12:10 -0700

    support matrix assign: A(:)=Val, A(B)=C, A(B)=c

--- src/io/lambdacloud/node/matrix/MatrixAssignNode.java
+++ src/io/lambdacloud/node/matrix/MatrixAssignNode.java
@@ -12,6 +12,7 @@ import Jama.Matrix;
 import io.lambdacloud.BytecodeSupport;
 import io.lambdacloud.MethodGenHelper;
 import io.lambdacloud.node.ExprNode;
+import io.lambdacloud.node.FuncCallNode;
 import io.lambdacloud.node.RangeNode;
 import io.lambdacloud.node.Tools;
 import io.lambdacloud.node.VariableNode;
@@ -55,11 +56,73 @@ public class MatrixAssignNode extends ExprNode {
 		@Override
 		public void genCode(MethodGenHelper mg) {
 			Type myType = this.getType();
-			var.genCode(mg);
+
 			if(this.indices.size() > 2) {
 				throw new UnsupportedOperationException();
 			}
+
+			//A(:)=Value
+			if(this.indices.size() == 1 && null == this.indices.get(0).idxS) {
+				var.genCode(mg);
+
+				mg.visitInsn(Opcodes.ICONST_0);
+				var.genCode(mg);
+				mg.visitMethodInsn(INVOKEVIRTUAL, "Jama/Matrix", "getRowDimension", "()I", false);
+				mg.visitInsn(Opcodes.ICONST_1);
+				mg.visitInsn(Opcodes.ISUB);
+				mg.visitInsn(Opcodes.ICONST_0);
+				var.genCode(mg);
+				mg.visitMethodInsn(INVOKEVIRTUAL, "Jama/Matrix", "getColumnDimension", "()I", false);
+				mg.visitInsn(Opcodes.ICONST_1);
+				mg.visitInsn(Opcodes.ISUB);
+				this.value.genCode(mg);
+				if(this.value.getType().getDescriptor().equals(Type.getType(Jama.Matrix.class).getDescriptor())) {
+					mg.visitMethodInsn(INVOKEVIRTUAL, "Jama/Matrix", "setMatrix", "(IIIILJama/Matrix;)V", false);
+				} else {
+					Tools.insertConversionInsn(mg, this.value.getType(), Type.DOUBLE_TYPE);
+					mg.visitMethodInsn(Opcodes.INVOKESTATIC, BytecodeSupport.getMyName(), "setMatrix", "(LJama/Matrix;IIIID)V", false);
+				}
+				if (genLoadInsn) {
+					mg.visitIntInsn(myType.getOpcode(Opcodes.ILOAD), var.getLVTIndex(myType.getDescriptor()));
+				}
+				return;
+			}
 			
+			//A(B)=Value
+			if(this.indices.size() == 1 && null != this.indices.get(0).idxS) {
+				ExprNode idx = this.indices.get(0).idxS;
+				if(idx.getType().getDescriptor().equals(Type.getType(Jama.Matrix.class).getDescriptor())) {
+					var.genCode(mg);
+					idx.genCode(mg);
+					this.value.genCode(mg);
+					if(this.value.getType().getDescriptor().equals(Type.getType(Jama.Matrix.class).getDescriptor())) {
+						mg.visitMethodInsn(Opcodes.INVOKESTATIC, BytecodeSupport.getMyName(), "setMatrix", "(LJama/Matrix;LJama/Matrix;LJama/Matrix;)LJama/Matrix;", false);
+					} else {
+						Tools.insertConversionInsn(mg, this.value.getType(), Type.DOUBLE_TYPE);
+						mg.visitMethodInsn(Opcodes.INVOKESTATIC, BytecodeSupport.getMyName(), "setMatrix", "(LJama/Matrix;LJama/Matrix;D)LJama/Matrix;", false);
+					}
+					if (genLoadInsn) {
+						mg.visitIntInsn(myType.getOpcode(Opcodes.ILOAD), var.getLVTIndex(myType.getDescriptor()));
+					}
+				} else if(idx.getType().getSort() != Type.OBJECT && idx.getType().getSort() != Type.ARRAY) {
+					var.genCode(mg);
+					idx.genCode(mg);
+					if(MatrixAccessNode.INDEX_BASE == 1) {
+						mg.visitInsn(Opcodes.ICONST_1);
+						mg.visitInsn(Opcodes.ISUB);
+					}
+					Tools.insertConversionInsn(mg, idx.getType(), Type.INT_TYPE);
+					this.value.genCode(mg);
+					Tools.insertConversionInsn(mg, this.value.getType(), Type.DOUBLE_TYPE);
+					mg.visitMethodInsn(Opcodes.INVOKESTATIC, BytecodeSupport.getMyName(), "setElement", "(LJama/Matrix;ID)V", false);
+				}
+				if (genLoadInsn) {
+					mg.visitIntInsn(myType.getOpcode(Opcodes.ILOAD), var.getLVTIndex(myType.getDescriptor()));
+				}
+				return;
+			}
+			
+			var.genCode(mg);
 			int type = 0x0;
 			for(int i=this.indices.size()-1; i>=0; i--) {
 				IndexPair ip = this.indices.get(i);
@@ -84,11 +147,11 @@ public class MatrixAssignNode extends ExprNode {
 						}
 						if(i == 1) {
 							ip.idxS.genCode(mg);
-							Tools.insertConversionInsn(mg, ip.idxS.getType(), Type.INT_TYPE);
+							mg.visitMethodInsn(Opcodes.INVOKESTATIC, BytecodeSupport.getMyName(), "convert", "(LJama/Matrix;)[I", false);
 							type |= 0x1;
 						} else if(i == 0) {
 							ip.idxS.genCode(mg);
-							Tools.insertConversionInsn(mg, ip.idxS.getType(), Type.INT_TYPE);
+							mg.visitMethodInsn(Opcodes.INVOKESTATIC, BytecodeSupport.getMyName(), "convert", "(LJama/Matrix;)[I", false);
 							type |= 0x2;
 						} else
 							throw new RuntimeException();
@@ -112,8 +175,8 @@ public class MatrixAssignNode extends ExprNode {
 					}
 				}
 			}
+			this.value.genCode(mg);
 			if(this.value.getType().getDescriptor().equals(Type.getType(Jama.Matrix.class).getDescriptor())) {
-				this.value.genCode(mg);
 				if(type == 0x0)
 					mg.visitMethodInsn(INVOKEVIRTUAL, "Jama/Matrix", "setMatrix", "(IIIILJama/Matrix;)V", false);
 				else if(type == 0x1)
@@ -124,7 +187,6 @@ public class MatrixAssignNode extends ExprNode {
 					mg.visitMethodInsn(INVOKEVIRTUAL, "Jama/Matrix", "setMatrix", "([I[ILJama/Matrix;)V", false);
 				}
 			else {
-				this.value.genCode(mg);
 				Tools.insertConversionInsn(mg, this.value.getType(), Type.DOUBLE_TYPE);
 				if(type == 0x0)
 					mg.visitMethodInsn(Opcodes.INVOKESTATIC, BytecodeSupport.getMyName(), "setMatrix", "(LJama/Matrix;IIIID)V", false);
@@ -139,7 +201,7 @@ public class MatrixAssignNode extends ExprNode {
 				mg.visitIntInsn(myType.getOpcode(Opcodes.ILOAD), var.getLVTIndex(myType.getDescriptor()));
 			}
 		}
-
+		
 		@Override
 		public Type getType(Deque<Object> stack) {
 			return Type.getType(Jama.Matrix.class);
