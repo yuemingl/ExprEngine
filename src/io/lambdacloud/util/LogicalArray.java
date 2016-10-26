def34fa025457473657fa4d4c133f0784fdc5447
Nathan Liu <cs_opo@hotmail.com>
2016-10-26 11:28:47 -0700

    Add class LogicalArray

---
+++ src/io/lambdacloud/util/LogicalArray.java
@@ -0,0 +1,13 @@
+package io.lambdacloud.util;
+
+public class LogicalArray extends ObjectArray {
+
+	public LogicalArray(int m, int n) {
+		super(m, n);
+	}
+	
+	public LogicalArray(Object[][] data) {
+		super(data);
+	}
+
+}
