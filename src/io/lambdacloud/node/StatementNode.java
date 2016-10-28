9893d3f0675722c5ec65adc1509d1bed5a114b1c
Nathan Liu <cs_opo@hotmail.com>
2016-10-28 14:05:22 -0700

    Add StatementNode as a corresponding to statement block

---
+++ src/io/lambdacloud/node/StatementNode.java
@@ -0,0 +1,35 @@
+package io.lambdacloud.node;
+
+import java.util.ArrayList;
+import java.util.Deque;
+
+import org.objectweb.asm.Type;
+
+import io.lambdacloud.MethodGenHelper;
+
+public class StatementNode extends ExprNode {
+	public ArrayList<ExprNode> exprs = new ArrayList<ExprNode>();
+
+	@Override
+	public void genCode(MethodGenHelper mg) {
+		for(int i=exprs.size()-1; i>=0; i--) {
+			exprs.get(i).genCode(mg);
+		}
+	}
+
+	@Override
+	public Type getType(Deque<Object> stack) {
+		for(int i=exprs.size()-1; i>=0; i--) {
+			Type ty = exprs.get(i).getType(stack);
+			if(null != ty) return ty;
+		}
+		return null;
+	}
+
+	@Override
+	public void updateType(Deque<Object> stack) {
+		for(int i=exprs.size()-1; i>=0; i--) {
+			exprs.get(i).updateType(stack);
+		}
+	}
+}
